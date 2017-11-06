71e02f4e7dcc012c5e6a1f19895c62c5373d74ba
Frank_Qi <sqiclemson@gmail.com>
2017-11-05 21:23:17 -0500

    Update HIT_Result_Analysis.py

--- HIT_Result_Analysis.py
+++ HIT_Result_Analysis.py
@@ -38,6 +38,7 @@ choice = 4 # of choices
 N = 100 # of questions
 
 
+worker_tasks_number = {}
 result = {}
 annotation_result = {}
 workers = []
@@ -59,15 +60,10 @@ with open('2993796.csv', 'r') as csvfile:
         for i in range(1, questions_per_HIT+1):#(1,11) = question per HIT
             tweets[line['Input.id'+str(i)]] = line['Input.tweet'+str(i)]
             if line['Input.id' + str(i)] not in result.keys():
-                if line['WorkerId'] != 'A2HWLIJP5OVFD5':
-                    result[line['Input.id' + str(i)]] = {line['WorkerId']: line['Answer.attitude_' + str(i)]}
-                else:
-                    result[line['Input.id' + str(i)]] = {'A312KFQ78WB3A4': line['Answer.attitude_' + str(i)]}
+                result[line['Input.id' + str(i)]] = {line['WorkerId']: line['Answer.attitude_' + str(i)]}
             else:
-                if line['WorkerId'] != 'A2HWLIJP5OVFD5':
-                    result[line['Input.id' + str(i)]].update({line['WorkerId']: line['Answer.attitude_' + str(i)]})
-                else:
-                    result[line['Input.id' + str(i)]].update({'A312KFQ78WB3A4': line['Answer.attitude_' + str(i)]})
+                result[line['Input.id' + str(i)]].update({line['WorkerId']: line['Answer.attitude_' + str(i)]})
+
 
 
 
@@ -96,6 +92,20 @@ for i in result.keys():
     x = find_major(list(result[i].values()))
     if len(set(result[i].values())) == 1:
         annotation_result[i] = 'unanimous | ' + x
+        for j in result[i].keys():
+            if j not in workers_list:
+                workers_list.append(j)
+                if result[i][j] != x:
+                    workers.append({j: {i: 'inlier'}})
+                else:
+                    workers.append({j: {i: 'inlier'}})
+            else:
+                for k in workers:
+                    if list(k.keys())[0] == j:
+                        if result[i][j] != x:
+                            k[j].update({i: 'inlier'})
+                        else:
+                            k[j].update({i: 'inlier'})
     else:
         if x == 'undecided':
             annotation_result[i] = x + ' | ' + x
@@ -125,21 +135,28 @@ for i in result.keys():
                                 k[j].update({i: 'inlier'})
 
 
+
+
 #-------------------------------------------------------------------------#
 #Print and Write to csv file
 tweet_result = [['tweet_id', 'decision_type', 'decsiion', 'tweet']] # Header for tweet analysis report
-worker_result = [['worker_id','# of outliers', 'consistent percentage']] # Header for worker analysis report
+worker_result = [['worker_id','# of outliers', 'Questions Done', 'consistent percentage']] # Header for worker analysis report
 for i in annotation_result.keys():
     tweet_result.append([i, annotation_result[i].split(' | ')[0], annotation_result[i].split(' | ')[1], tweets[i]])
     print(i + ' | ' + annotation_result[i] + ' | ' + tweets[i])
+
+
+for i in workers:
+    worker_tasks_number[list(i.keys())[0]] = len(list(i.values())[0].keys())
+
 for i in workers:
     t = 0
     for k in i[list(i.keys())[0]].values():
         if k == 'outlier':
             t += 1
-    percentage = t * 1.0 / N
-    worker_result.append([list(i.keys())[0], t, 1 - percentage])
-    print('Worker_Id: ' + list(i.keys())[0] + ' | # of outliers = ' + str(t) + ' | consistent percentage: ' + str(1 - percentage) )
+    percentage = t * 1.0 / worker_tasks_number[list(i.keys())[0]]
+    worker_result.append([list(i.keys())[0], t, worker_tasks_number[list(i.keys())[0]], 1 - percentage])
+    print('Worker_Id: ' + list(i.keys())[0] + ' | # of outliers = ' + str(t) + ' | Total Questions Done: ' + str(worker_tasks_number[list(i.keys())[0]]) + ' | consistent percentage: ' + str(1 - percentage) )
 
 for i in temp_p.values():
     p.append(i/(N*1.0*n))
